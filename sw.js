bca3c0d4afa19ffa38d87282e2efb78549032d73
zenghongtu <zenghongtu@gmail.com>
2019-03-14 02:46:12 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8ac17988794d1046bcc2.js"
+    "url": "webpack-runtime-a9082cb257958705dbbc.js"
   },
   {
     "url": "styles.d7d4b5ed1c4dbcc9a937.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "452804047c7bd606f9654aeff16ddc18"
+    "revision": "45930efb2f3d2ad7dd31c286c3eabf19"
   },
   {
     "url": "component---src-pages-404-js-faa991146b0e5b784b7d.js"
