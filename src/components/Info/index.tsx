dbbce48c2b82f5923fc81123c9a9bf66f316c127
zenghongtu <zenghongtu@gmail.com>
2022-07-13 14:32:05 +0800

    update info

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -3,17 +3,47 @@ import './index.css';
 
 const index = () => {
   return (
-    <div className="px-7 py-10">
+    <div className="px-28 py-40">
       <div className="content">
         <div className="avatar-header">
           <a href="/">
             <img className="avatar" src={avatar} />
           </a>
-
           <div>
             <h1 className="name">Jason Zeng</h1>
           </div>
         </div>
+        <p className="py-40">
+          <a className="link" href="https://github.com/zenghongtu">
+            GitHub
+          </a>
+          {' · '}
+          <a className="link" href="https://blog.jasonzeng.dev">
+            Blog
+          </a>
+          {' · '}
+          <a className="link" href="https://blog.jasonzeng.dev/projects">
+            Projects
+          </a>
+        </p>
+        <details className="mb-16">
+          <summary style={{ cursor: 'pointer', userSelect: 'none' }}>
+            Metrics
+          </summary>
+          <div>
+            <img src="./metrics.svg"></img>
+          </div>
+        </details>
+        <div>
+          <iframe
+            className="gist-iframe"
+            src="https://gist.github.com/zenghongtu/4e438abfd13a9ec2c2f2a882bc3c8058.pibb"
+          ></iframe>
+        </div>
+        <iframe
+          className="gist-iframe"
+          src="https://gist.github.com/zenghongtu/0886c55aba38694add4b38b17d9cb4cb.pibb"
+        ></iframe>
       </div>
     </div>
   );
